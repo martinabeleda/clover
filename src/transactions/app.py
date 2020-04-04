a16ba272885496a13390373094571d2e05bf727b
martinabeleda <martin.abeleda@gmail.com>
2020-04-04 19:11:40 +1100

    Configure pylint, black, mypy and isort with VSCode

--- src/transactions/app.py
+++ src/transactions/app.py
@@ -1,16 +1,18 @@
 from pathlib import Path
 
-import markdown
 from flask import Flask
+import markdown
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    path = Path('/app/README.md')
-    with path.open('r') as markdown_file:
+    path = Path("/app/README.md")
+    with path.open("r") as markdown_file:
         content = markdown_file.read()
         return markdown.markdown(content)
 
+
 def main():
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
